bd609a660f6f25e723766198e436233f53d49461
quiland15 <quiland86@gmail.com>
2025-04-07 19:39:27 +0800

    mengganti localhost di config menjadi ippublic

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 	// define path constants
-    define("APP_PATH", 'http://localhost/unkpresent/public');
+    define("APP_PATH", 'http://103.226.138.171/unkpresent/public');
 
     // define database config
     
